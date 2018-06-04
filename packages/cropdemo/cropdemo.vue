ad37bcf9821d0546d8ee13673943038f91baab37
happyplus-chen <happyplus_chen@163.com>
2018-06-04 23:19:57 +0800

    CROP

--- packages/cropdemo/cropdemo.vue
+++ packages/cropdemo/cropdemo.vue
@@ -1,33 +1,120 @@
+
+<style scoped>
+.img-container {
+  margin: auto;
+}
+.img-container > img {
+  max-width: 100%;
+}
+.opration {
+ text-align: center;
+  background-color: rgba(55, 55, 55, 0.5);
+  position: relative;
+  padding-top: 4px;
+  height: 36px;
+  line-height: 36px;
+}
+.opration span {
+  width: 26px;
+  height: 26px;
+  /* margin-left: 4px; */
+  display: inline-block;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 50%;
+  background-color: #000;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.opration span:hover{
+    background-color: rgba(0, 0, 0, 0.8);
+}
+
+.opration span.max {
+   width: 30px;
+  height: 30px;
+}
+
+</style>
+<style scoped>
+.crop-modal-mask {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(55, 55, 55, 0.5);
+  height: 100%;
+  z-index: 1000;
+}
+.crop-modal-warp {
+  position: fixed;
+  overflow: auto;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
+  -webkit-overflow-scrolling: touch;
+  outline: 0;
+}
+.crop-modal {
+  position: relative;
+  top: 100px;
+  margin: 0 auto 50px;
+  background: #fff;
+  border-radius: 2px;
+  /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3); */
+  box-sizing: border-box;
+  width: 50%;
+}
+.moda-close {
+  position: absolute;
+  top: -40px;
+  right: -40px;
+  width: 80px;
+  height: 80px;
+  cursor: pointer;
+  border-radius: 50%;
+  background-color: #000;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.moda-close::before {
+  content: 'X';
+  position: absolute;
+  bottom: 13px;
+  left: 22px;
+  color: #fff;
+}
+</style>
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-9">
-        <!-- <h3>Demo:</h3> -->
-        <div class="img-container">
-          <img :src="imgsrc" alt="Picture" ref="img">
-        </div>
-      </div>
-      <div class="col-md-3">
-        <!-- <h3>Preview:</h3> -->
-        <div class="docs-preview clearfix">
-          <div class="img-preview preview-lg"></div>
-          <!-- <div class="img-preview preview-md"></div>
-          <div class="img-preview preview-sm"></div>
-          <div class="img-preview preview-xs"></div> -->
-        </div>
-        <div>
-          <button ref="browse">upload</button>
-          <button @click="crop">crop</button>
-          <a :href="cropimg" download="cropped.jpg">download</a>
+  <div class="">
+    <div v-show="show" class="crop-modal-mask">
+      <div class="moda-close" @click.native="show=false">X</div>
+      <div class="crop-modal-warp">
+        <div class="crop-modal">
+          <div class="crop-container">
+            <div class="img-container">
+              <img :src="imgsrc" alt="Picture" ref="img">
+            </div>
+          </div>
+          <ul class="opration">
+            <span ref="browse">U</span>
+            <span @click="crop">1</span>
+            <span @click="crop">X</span>
+            <span @click="crop">C</span>
+            <span class="max" @click="crop">R</span>
+            <span @click="crop">S</span>
+            <span @click="crop">L</span>
+            <span @click="crop">C</span>
+            <span :href="cropimg" download="cropped.jpg">D</span>
+          </ul>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import './css/bootstrap.min.css'
 import './css/cropper.css'
 import plupload from 'plupload/js/plupload.full.min.js'
 import Cropper from './js/cropper.js'
@@ -36,6 +123,7 @@ const mOxie = plupload.moxie
 export default {
   data() {
     return {
+      show: true,
       cropper: null,
       cropimg: '',
       imgsrc: 'static/images/picture.jpg',
@@ -73,7 +161,7 @@ export default {
       } else {
         var preloader = new mOxie.image.Image()
         preloader.onload = function() {
-          preloader.downsize(300, 300) // 先压缩一下要预览的图片,宽300，高300
+          // preloader.downsize(300, 300) // 先压缩一下要预览的图片,宽300，高300
           var imgsrc =
             preloader.type === 'image/jpeg'
               ? preloader.getAsDataURL('image/jpeg', 80)
@@ -139,65 +227,4 @@ export default {
   }
 }
 </script>
-<style scoped>
-.img-container,
-.img-preview {
-  background-color: #f7f7f7;
-  text-align: center;
-  width: 100%;
-}
 
-.img-container {
-  margin-bottom: 1rem;
-  max-height: 497px;
-  /* min-height: 200px; */
-  width: 500px;
-  /* height: 200px; */
-}
-
-@media (min-width: 768px) {
-  /* .img-container {
-    min-height: 497px;
-  } */
-}
-
-.img-container > img {
-  max-width: 100%;
-}
-
-.docs-preview {
-  margin-right: -1rem;
-}
-
-.img-preview {
-  float: left;
-  margin-bottom: 0.5rem;
-  margin-right: 0.5rem;
-  overflow: hidden;
-}
-
-.img-preview > img {
-  max-width: 100%;
-}
-
-.preview-lg {
-  height: 9rem;
-  width: 16rem;
-}
-
-.preview-md {
-  height: 4.5rem;
-  width: 8rem;
-}
-
-.preview-sm {
-  height: 2.25rem;
-  width: 4rem;
-}
-
-.preview-xs {
-  height: 1.125rem;
-  margin-right: 0;
-  width: 2rem;
-}
-</style>
